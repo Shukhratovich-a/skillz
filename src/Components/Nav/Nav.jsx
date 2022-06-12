e696cc94985b549459a33232ef3915bb760aba62
Shukhratovich-a <shukhratovich138@gmail.com>
2022-06-13 01:47:52 +0500

    Finish header

--- src/Components/Nav/Nav.jsx
+++ src/Components/Nav/Nav.jsx
@@ -9,9 +9,37 @@ const Nav = () => {
   return (
     <nav className="nav">
       <div className="container">
-        <Link to={"/"}>
+        <Link className="nav__logo__link" to={"/"}>
           <Logo />
         </Link>
+
+        <ul className="nav__list">
+          <li className="nav__item">
+            <a className="nav__link" href="#link" title="Courses">
+              Courses
+            </a>
+          </li>
+          <li className="nav__item">
+            <a className="nav__link" href="#link" title="Free Workshops">
+              Free Workshops
+            </a>
+          </li>
+          <li className="nav__item">
+            <a className="nav__link" href="#link" title="Blog">
+              Blog
+            </a>
+          </li>
+          <li className="nav__item">
+            <a className="nav__link" href="#link" title="About">
+              About
+            </a>
+          </li>
+          <li className="nav__item">
+            <a className="nav__link" href="#link" title="Contact">
+              Contact
+            </a>
+          </li>
+        </ul>
 
         <button className="nav__button">
           <Basket />
