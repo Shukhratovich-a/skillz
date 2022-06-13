78616988e852b767c50eb91494384bca853d42d2
Shukhratovich-a <shukhratovich138@gmail.com>
2022-06-13 08:44:46 +0500

    Finish recommendations section

--- src/Pages/Home/Home.jsx
+++ src/Pages/Home/Home.jsx
@@ -1,11 +1,13 @@
 import Hero from "../../Components/Hero/Hero";
 import Favorites from "../../Components/Favorites/Favorites";
+import Recommendations from "../../Components/Recommendations/Recommendations";
 
 const Home = () => {
   return (
     <main className="main">
       <Hero />
       <Favorites />
+      <Recommendations />
     </main>
   );
 };
